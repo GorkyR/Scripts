6f2b81363bb7106ee66dc877cd809aefde0e66c3
GorkyR <1gorky3rojas@gmail.com>
2017-03-31 12:21:17 -0400

    Update scripts

--- Soundcloud Expose artwork image.user.js
+++ Soundcloud Expose artwork image.user.js
@@ -1,9 +1,19 @@
 // ==UserScript==
 // @name         Soundcloud | Expose artwork image
-// @version      0.3
+// @version      0.5
 // @author       GorkyR
 // @match        https://soundcloud.com/*
 // ==/UserScript==
+
+var curUrl = "";
+
+function checkUrlChange(){
+    if (curUrl != window.location.href){
+        curUrl = window.location.href;
+        console.log('Location changed!');
+        exposeArtwork();
+    }
+}
 
 function exposeArtwork() {
     var a = document.getElementsByClassName('listenArtworkWrapper__artwork')[0];
@@ -18,9 +28,5 @@ function exposeArtwork() {
 
 (function(){
     'use strict';
-    var observer = new MutationObserver(function(mutations){
-        exposeArtwork();
-    });
-    var observerConfig = { childList: true, subtree: true };
-    observer.observe(document.body, config);
+    setInterval(checkUrlChange, 1000);
 })();
