42cc09af49c7cc3fbcd0359d96b210088e6c5a61
GorkyR <1gorky3rojas@gmail.com>
2017-04-10 10:23:34 -0400

    Update and add scripts

---
+++ Enable interaction.user.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         * | Enable interaction
+// @namespace    http://github.com/GorkyR
+// @version      0.2
+// @description  Re-enables context menu and text selection
+// @author       GorkyR
+// @match        *://*/*
+// @grant        none
+// ==/UserScript==
+
+function allowTextSelection() {
+	// from https://alanhogan.com/code/text-selection-bookmarklet
+	var style = document.createElement('style');
+	style.type = 'text/css';
+	style.innerHTML = '*,p,div{user-select:text !important;-moz-user-select:text !important;-webkit-user-select:text !important;}';
+	document.head.appendChild(style);
+	var elArray = document.body.getElementsByTagName('*');
+	for (var i = 0; i < elArray.length; i++) {
+		var el = elArray[i];
+		el.onselectstart = el.ondragstart = el.ondrag = el.oncontextmenu = el.onmousedown = el.onmouseup = function () { return true; };
+		if (el instanceof HTMLInputElement && ['text', 'password', 'email', 'number', 'tel', 'url'].indexOf(el.type.toLowerCase()) > -1) {
+			el.removeAttribute('disabled');
+			el.onkeydown = el.onkeyup = function () { return true; };
+		}
+	}
+
+}
+
+(function () {
+	'use strict';
+	console.log('Allow context menu');
+	document.onmousedown = document.onclick = document.oncontextmenu = null;
+	console.log('Allow text selection');
+	allowTextSelection();
+})();
---
+++ Reddit - Sort random.user.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         Reddit | Sort random
+// @namespace    http://github.com/GorkyR
+// @version      0.2
+// @description  Forces random sorting of comments
+// @author       GorkyR
+// @match        *://*.reddit.com/*/comments/*
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    URL = window.location.href;
+    var pos = URL.indexOf('sort=');
+    if (pos === -1)
+        window.location.href += (URL.indexOf('/?') == -1? '?' : '&') + 'sort=random';
+    else if (URL.substring(pos + 5, pos + 11) != 'random')
+        window.location.href = URL.substring(0, pos + 5) + 'random&' + URL.substring(pos + 12);
+})();
