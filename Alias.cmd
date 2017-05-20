28a255cc254b20e45a93d127f64ce66fe2248060
GorkyR <1gorky3rojas@gmail.com>
2017-05-20 19:04:34 -0400

    Update scripts

--- Alias.cmd
+++ Alias.cmd
@@ -1,8 +1,11 @@
 @echo off
 rem Alias syntax =^> alias newCommandName=command
+set pwd=%cd%
+cd %userprofile%\Documents
 for /f "tokens=1* delims=^=" %%a in ("%*") do (
 rem writes the command to the newCommandName batch file 
 echo @%%b %%* > %%a.cmd
 rem hides the file
 attrib +h +s %%a.cmd
 )
+cd %pwd%
